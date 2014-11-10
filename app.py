f2b39faa80b85215155b13cb077db3c268065e03
Adriel Mendoza <adrielmendoza97@gmail.com>
2014-11-10 11:44:02 -0500

    Tried pulling out the text

--- app.py
+++ app.py
@@ -8,22 +8,34 @@ app=Flask(__name__)
 def index():
     return "this is the index page"
 
-@app.route("/tagged/")
-def tag(tag="monkeys"):
+@app.route("/tagged")
+def tag(tag="apple"):
     url = "http://api.tumblr.com/v2/tagged?tag=%s&api_key=dw6tmMQNuJU1I9jM6NPCuyPMwgYWKVlJGXZpQm1fWgMjEa9zxh"
     url = url%(tag)
     request = urllib.urlopen(url)
-    s = request.read()
-    result = json.loads(s)
+    st = request.read()
+    result = json.loads(st)
     retstring = ""
+    retstring1 = ""
+    retstring2 = ""
     for s in result["response"]:
         print(s)
+        #images
         try: 
-            retstring = retstring + "<img src=%s>"%(s['photos'][0]['original_size']['url'])
-            print(retstring)
+            retstring1 = retstring1 + "<img src=%s>"%(s['photos'][0]['original_size']['url'])
+            ##print(retstring1)
         except:
             pass
-    return retstring
+        #text
+        try:
+            retstring2 = retstring2 + s['text'][0]
+            print s['text'][0]
+        except:
+            pass
+        retstring = retstring + retstring1 + retstring2
+        return retstring2
+                
+    #return retstring2
 
 
 if __name__=="__main__":
