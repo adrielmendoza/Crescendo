63af55ed7e057b799d27db6adfffbc9aa5c0801d
Adriel Mendoza <adrielmendoza97@gmail.com>
2014-11-11 21:54:11 -0500

    Struggling with html

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, url_for, redirect, render_template
 import urllib
 import json
+import soundcloud
 
 app=Flask(__name__)
 
@@ -10,32 +11,53 @@ def index():
 
 @app.route("/tagged")
 def tag(tag="apple"):
-    url = "http://api.tumblr.com/v2/tagged?tag=%s&api_key=dw6tmMQNuJU1I9jM6NPCuyPMwgYWKVlJGXZpQm1fWgMjEa9zxh"
-    url = url%(tag)
+    url = "http://api.tumblr.com/v2/tagged?tag=" + tag + "&api_key=dw6tmMQNuJU1I9jM6NPCuyPMwgYWKVlJGXZpQm1fWgMjEa9zxh"
     request = urllib.urlopen(url)
     st = request.read()
     result = json.loads(st)
     retstring = ""
-    retstring1 = ""
-    retstring2 = ""
     for s in result["response"]:
         print(s)
         #images
         try: 
-            retstring1 = retstring1 + "<img src=%s>"%(s['photos'][0]['original_size']['url'])
+            retstring = retstring + "<img src=%s>"%(s['photos'][0]['original_size']['url'])
             ##print(retstring1)
         except:
             pass
-        #text
-        try:
-            retstring2 = retstring2 + s['text'][0]
-            print s['text'][0]
-        except:
-            pass
-        retstring = retstring + retstring1 + retstring2
-        return retstring2
+    return retstring
                 
     #return retstring2
+#@app.route("/play")
+#def play(tag="apple"):
+    #tracklist = ""
+
+    # create a client object with your app credentials
+    #client = soundcloud.Client(client_id='5c46cd90fc27e327c5d396d4c2cd2acc')
+
+    #pull tracks using query apple
+    #tracks = client.get('/tracks', q=tag)
+
+    # get a tracks oembed data
+    #for track in tracks:
+        #print track
+        #tracklist = tracklist + (client.get('/oembed', url=track))['html']
+
+    #return tracklist
+
+@app.route("/test")
+def o(tag="hello"):
+
+    # create a client object with your app credentials
+    client = soundcloud.Client(client_id='5c46cd90fc27e327c5d396d4c2cd2acc')
+
+    # get a tracks oembed data
+    track_url = 'http://soundcloud.com/forss/flickermood'
+    embed_info = client.get('/oembed', url=track_url)
+    print embed_info
+    # print the html for the player widget
+    return render_template('results.html', stuff = embed_info)
+
+
 
 
 if __name__=="__main__":
