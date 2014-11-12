35a48fb0e4d1c37c2406e741ecb97c1321ad2a4c
Adriel Mendoza <adrielmendoza97@gmail.com>
2014-11-12 01:24:21 -0500

    Added aesthetics

--- app.py
+++ app.py
@@ -8,13 +8,23 @@ app=Flask(__name__)
 @app.route("/")
 def index():
     #home page
-    render_template("home.html")
+    query = request.args.get("query")
+    name = request.args.get("name")
+    start = request.args.get("start")
+    if(query != "" and start == "Let's do this!"):
+        return redirect("/generate/" + query + "_" + name)
+    return render_template("home.html")
+
+@app.route("/about")
+def about():
+    #about page
+    return render_template("about.html")
                 
 @app.route("/generate")
-@app.route("/generate/<tag>")
-def generate(tag):
+@app.route("/generate/<query>_<name>")
+def generate(query, name):
     #IMAGE -----------------------------------------------------------------
-    url = "http://api.tumblr.com/v2/tagged?tag=" + tag + "&api_key=dw6tmMQNuJU1I9jM6NPCuyPMwgYWKVlJGXZpQm1fWgMjEa9zxh"
+    url = "http://api.tumblr.com/v2/tagged?tag=" + query + "&api_key=dw6tmMQNuJU1I9jM6NPCuyPMwgYWKVlJGXZpQm1fWgMjEa9zxh"
     request = urllib.urlopen(url)
     st = request.read()
     result = json.loads(st)
@@ -30,10 +40,10 @@ def generate(tag):
     client = soundcloud.Client(client_id='5c46cd90fc27e327c5d396d4c2cd2acc')
 
     #pulls tracks depending on the tag
-    tracks = client.get('/tracks', q=tag)
+    tracks = client.get('/tracks', q=query)
 
     #renders result.html and passes first track passed on query
-    return render_template('results.html', image = retstring, stuff = tracks[0])
+    return render_template('results.html', query = query, name = name, image = retstring, stuff = tracks[0])
 
 
 
